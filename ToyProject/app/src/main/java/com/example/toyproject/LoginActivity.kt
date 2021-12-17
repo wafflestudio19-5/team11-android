28c859553e9846b91e88a784f55cb59feceecf8d
정현 홍 <hongjh999@gmail.com>
2021-12-17 17:52:00 +0900

    login activity에 mainactivity가 binding되어 있어서 수정함. margin과 padding 값 조정

--- ToyProject/app/src/main/java/com/example/toyproject/LoginActivity.kt
+++ ToyProject/app/src/main/java/com/example/toyproject/LoginActivity.kt
@@ -2,15 +2,15 @@ package com.example.toyproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.toyproject.databinding.ActivityMainBinding
+import com.example.toyproject.databinding.ActivityLoginBinding
 
 class LoginActivity:AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
