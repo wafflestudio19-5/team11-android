786305ca234169c24892b41e9de4005d3dbc4024
sungminjo02 <jsm0424@snu.ac.kr>
2021-12-27 02:35:53 +0900

    상하단 바와 fragment 추가

--- ToyProject/app/src/main/java/com/example/toyproject/ui/main/MainActivity.kt
+++ ToyProject/app/src/main/java/com/example/toyproject/ui/main/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.edit
+import androidx.core.content.res.ResourcesCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.example.toyproject.R
 import com.example.toyproject.databinding.ActivityMainBinding
 import com.example.toyproject.network.Service
@@ -12,10 +14,13 @@ import com.example.toyproject.ui.login.LoginActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import java.lang.IllegalStateException
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -30,12 +35,33 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth : FirebaseAuth
     private lateinit var mGoogleSignInClient : GoogleSignInClient
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //하단 바
+        viewPager = binding.pager
+        tabLayout = binding.tabLayout
+
+        val pagerAdapter = ViewpagerFragmentAdapter(this)
+        viewPager.adapter = pagerAdapter
+
+        TabLayoutMediator(tabLayout, viewPager){
+                tab: TabLayout.Tab, i: Int ->
+            tab.icon = when(i){
+                0-> ResourcesCompat.getDrawable(resources, R.drawable.outline_home_24, null)
+                1-> ResourcesCompat.getDrawable(resources, R.drawable.outline_table_chart_24, null)
+                2-> ResourcesCompat.getDrawable(resources, R.drawable.outline_list_alt_24, null)
+                3-> ResourcesCompat.getDrawable(resources, R.drawable.outline_notifications_24, null)
+                4-> ResourcesCompat.getDrawable(resources, R.drawable.outline_policy_24, null)
+                else-> throw IllegalStateException("no tab")
+            }
+        }.attach()
 
 
         // Google 로그아웃 부분
