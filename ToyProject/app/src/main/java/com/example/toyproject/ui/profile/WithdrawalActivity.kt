e5145226ea934aea297868658fdf2e8414b0899f
정현 홍 <hongjh999@gmail.com>
2021-12-31 14:22:22 +0900

    탈퇴 시 한번 더 물어보는 팝업 추가

--- ToyProject/app/src/main/java/com/example/toyproject/ui/profile/WithdrawalActivity.kt
+++ ToyProject/app/src/main/java/com/example/toyproject/ui/profile/WithdrawalActivity.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import com.example.toyproject.databinding.ActivityWithdrawalBinding
@@ -24,7 +25,7 @@ class WithdrawalActivity: AppCompatActivity() {
         setContentView(binding.root)
 
         binding.withdrawalButton.setOnClickListener{
-            viewModel.withdrawal(binding.curPasswordInput.text.toString())
+            showDialog()
         }
 
         viewModel.response.observe(this, {
@@ -43,5 +44,19 @@ class WithdrawalActivity: AppCompatActivity() {
         viewModel.result.observe(this,{
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         })
+    }
+
+    private fun showDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setTitle("탈퇴 확인")
+            .setMessage("정말로 탈퇴하시겠습니까?")
+            .setPositiveButton("네") { _, _ ->
+                viewModel.withdrawal(binding.curPasswordInput.text.toString())
+            }
+            .setNegativeButton("아니오") { _, _ ->
+                Toast.makeText(applicationContext, "취소되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        val dialog = dialogBuilder.create()
+        dialog.show()
     }
 }
