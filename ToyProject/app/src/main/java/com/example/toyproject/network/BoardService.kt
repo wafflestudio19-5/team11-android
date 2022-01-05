320517f1177a55482512b8142b1a8316fd56538c
JuTaK97 <didwngus01@snu.ac.kr>
2022-01-05 14:46:35 +0900

    feature/comment 와 머지

--- ToyProject/app/src/main/java/com/example/toyproject/network/BoardService.kt
+++ ToyProject/app/src/main/java/com/example/toyproject/network/BoardService.kt
@@ -1,6 +1,7 @@
 package com.example.toyproject.network
 
 import com.example.toyproject.network.dto.*
+import retrofit2.Call
 import retrofit2.http.*
 
 interface BoardService {
@@ -46,11 +47,38 @@ interface BoardService {
         @Query("search") search: String
     ) : FetchArticlesByBoard
 
+    @GET("api/v1/board/{board_id}/article/{article_id}/")
+    suspend fun getArticleContent(
+        @Path("board_id") board_id : Int,
+        @Path("article_id") article_id : Int,
+    ) : ArticleContent
 
+    @POST("api/v1/board/{board_id}/article/{article_id}/comment/")
+    suspend fun addComment(
+        @Path("board_id") board_id : Int,
+        @Path("article_id") article_id : Int,
+        @Body param : CommentCreate
+    ) : CommentCreateResponse
 
+    @POST("api/v1/like/comment/{comment_id}/")
+    suspend fun likeComment(
+        @Path("comment_id") comment_id : Int
+    ) : LikeResponse
 
+    @POST("api/v1/like/article/{article_id}/")
+    fun likeArticle(
+        @Path("article_id") article_id : Int
+    ) : Call<LikeResponse>
 
+    @POST("api/v1/scrap/article/{article_id}/")
+    fun scrapArticle(
+        @Path("article_id") article_id: Int
+    ) : Call<ScrapResponse>
 
-
-
+    @DELETE("/api/v1/board/{board_id}/article/{article_id}/comment/{comment_id}/")
+    fun deleteComment(
+        @Path("board_id") board_id : Int,
+        @Path("article_id") article_id : Int,
+        @Path("comment_id") comment_id: Int
+    ) : Call<CommentDeleteResponse>
 }
