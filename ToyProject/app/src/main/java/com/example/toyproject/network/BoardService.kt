e0dd50bdd2bfa170c066a30e2176b4cc643a02b8
JuTaK97 <didwngus01@snu.ac.kr>
2022-01-05 14:38:45 +0900

    conflict 해결

--- ToyProject/app/src/main/java/com/example/toyproject/network/BoardService.kt
+++ ToyProject/app/src/main/java/com/example/toyproject/network/BoardService.kt
@@ -15,48 +15,37 @@ interface BoardService {
         @Query("offset") offset: Int,
         @Query("limit") limit: Int): FetchArticlesByBoard
 
+    @FormUrlEncoded
+    @POST("/api/v1/board/")
+    suspend fun createBoard(
+        @Field("name") name: String,
+        @Field("description") description: String,
+        @Field("allow_anonymous") allow_anonymous: Boolean,
+        @Field("type") type: Int
+    ): CreateBoard
 
+    @FormUrlEncoded
+    @POST("/api/v1/board/{board_id}/article/")
+    suspend fun createArticle(
+        @Path("board_id") board_id: Int,
+        @Field("title") title: String,
+        @Field("text") text: String,
+        @Field("is_anonymous") is_anonymous: Boolean,
+        @Field("is_question") is_question: Boolean
+    ): CreateArticle
 
+    @GET("/api/v1/board/")
+    suspend fun searchBoard(
+        @Query("search") search: String
+    ): FetchAllBoard
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @GET("/api/v1/board/{board_id}/article/")
+    suspend fun getArticlesByKeyword(
+        @Path("board_id") board_id: Int,
+        @Query("offset") offset: Int,
+        @Query("limit") limit: Int,
+        @Query("search") search: String
+    ) : FetchArticlesByBoard
 
     @GET("api/v1/board/{board_id}/article/{article_id}/")
     suspend fun getArticleContent(
