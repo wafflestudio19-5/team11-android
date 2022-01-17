1002181e6a3fdd78eabecf2a17509f31677425a1
JuTaK97 <didwngus01@snu.ac.kr>
2022-01-17 20:52:21 +0900

    시간표 목록 이중 RecyclerView 구현

--- ToyProject/app/src/main/java/com/example/toyproject/ui/main/tableFragment/TableFragment.kt
+++ ToyProject/app/src/main/java/com/example/toyproject/ui/main/tableFragment/TableFragment.kt
@@ -1,9 +1,12 @@
 package com.example.toyproject.ui.main.tableFragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.toyproject.databinding.FragmentTableBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +23,30 @@ class TableFragment : Fragment() {
     ): View {
         binding = FragmentTableBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // "RESULT_OK" :
+        val resultListener =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if(it.resultCode == AppCompatActivity.RESULT_OK) {
+
+                }
+            }
+
+        // 우상단 시간표 목룍 버튼
+        binding.tableButtonList.setOnClickListener {
+            val intent = Intent(activity, TableListActivity::class.java)
+            resultListener.launch(intent)
+        }
+        binding.tableButtonSetting.setOnClickListener {
+
+        }
+        binding.tableButtonAddClass.setOnClickListener {
+
+        }
     }
 
 }
---
+++ ToyProject/app/src/main/java/com/example/toyproject/ui/main/tableFragment/TableListAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.toyproject.ui.main.tableFragment
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.toyproject.databinding.FragmentTableSemesterCellBinding
+import com.example.toyproject.network.dto.table.Semester
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.toyproject.R
+import com.example.toyproject.network.dto.table.Schedule
+import java.lang.StringBuilder
+
+
+class TableListAdapter(private val context: Context) :
+    RecyclerView.Adapter<TableListAdapter.Holder>() {
+
+    private val match = arrayOf("1학기", "2학기", "여름학기", "겨울학기")
+
+    private var list: MutableList<Semester> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TableListAdapter.Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.fragment_table_semester_cell, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: TableListAdapter.Holder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    fun setSemesters(list : MutableList<Semester>) {
+        this.list = list
+    }
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        private val title = itemView?.findViewById<TextView>(R.id.table_list_semester_title)
+
+        fun bind(semester: Semester) {
+            val stringBuilder = StringBuilder()
+            stringBuilder.append(semester.year.toString())
+            stringBuilder.append("년 ")
+            stringBuilder.append(match[semester.season])
+            title?.text = stringBuilder.toString()
+
+            val recyclerView = itemView.findViewById<RecyclerView>(R.id.table_list_semester_recycler_view)
+            val layoutManager = LinearLayoutManager(context)
+
+            val mAdapter = TableListCellAdapter(context, semester.schedules.toMutableList())
+            recyclerView.adapter = mAdapter
+            recyclerView.layoutManager = layoutManager
+            // recyclerView.setHasFixedSize(true)
+        }
+    }
+
+
+
+}
